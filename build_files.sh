ebf02f599b86e82b5da1c5f3395aae57b44b2593
Paramdeep Singh <=>
2024-02-22 11:24:50 +0530

    vercel change

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python -m pip install -r requirement.txt
-python manage.py collectstatic --noinput --clear
+python3.12.0 -m pip install -r requirement.txt
+python3.12.0 manage.py collectstatic --noinput --clear
 echo "BIULD END"
